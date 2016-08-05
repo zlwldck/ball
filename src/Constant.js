11cb250cd3483a7880a99413001534b389c1ebfa
zlwldck <zlwldck@sina.com>
2016-08-05 17:25:33 +0800

    Add files via upload

---
+++ src/Constant.js
@@ -0,0 +1,15 @@
+var Constant = {
+	CANDY_WIDTH: 64,
+	CANDY_TYPE_COUNT:5,
+	MAP_SIZE: 10,
+	FALL_ACCELERATION: 30,
+
+	levels:[
+		{limitStep:30, targetScore:500},
+		{limitStep:25, targetScore:1500},
+		{limitStep:20, targetScore:2500},
+		{limitStep:20, targetScore:3500},
+		{limitStep:15, targetScore:4500},
+		{limitStep:15, targetScore:5500},
+	]
+}
---
+++ src/resource.js
@@ -0,0 +1,12 @@
+var res = {
+    s2_png : "res/2.png",
+    s3_png : "res/3.png",
+    s4_png : "res/4.png",
+    s5_png : "res/5.png",
+    bg_png : "res/bg.jpg",
+};
+
+var g_resources = [];
+for (var i in res) {
+    g_resources.push(res[i]);
+}
